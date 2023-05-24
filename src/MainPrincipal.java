0d0936065fe9147aee5bb1cd686119f941124601
Edu <79867404+Uardet99@users.noreply.github.com>
2023-05-24 11:47:50 +0200

    Implementando la opcion 'Ver perfil', sin funcionalidad

--- src/MainPrincipal.java
+++ src/MainPrincipal.java
@@ -9,13 +9,16 @@ public class MainPrincipal {
 
 		// Fase de pruebas, con esto podremos cambiar la aparincia de la interfaz por
 		// una que esta incluida en nuestras bibliotecas
+		
+//		Perfil perfil = new Perfil();
+		
 		try {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		} catch (Exception e) {
 			System.out.println("Error al establecer el look and feel de FlatLaf");
 			e.printStackTrace();
 		}
-
+ 
 		Panel_inicio p = new Panel_inicio();
 
 	}
